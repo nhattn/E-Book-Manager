9d7ca73f778cf1566a08e4ecc9d82e2e7e1fc32a
Trần Ngọc Nhật <anhchangthode22145@gmail.com>
2022-04-06 13:22:23 +0700

    Update handle add new book + style if no cover

--- app/views/manage.py
+++ app/views/manage.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from app import engine, RANDOM_UID
+from app import engine, RANDOM_UID, db
 from flask import render_template, abort, request, jsonify
 from ..models.book import Book
 from ..models.author import Author
+from datetime import datetime
 from .util import get_list_pages, is_async
 
 @engine.route('/')
@@ -41,3 +42,71 @@ def ebook(id=0):
         'recents' : query.items
     }
     return render_template('ebook.html', **argv)
+
+@engine.route('/newbook', methods=['POST'])
+def newbook():
+    if request.content_type and "application/json" in request.content_type:
+        data = request.get_json()
+    else:
+        data = request.form
+    name = data.get("name","").strip()
+    if not name:
+        return jsonify({
+            "error":"Name is not valid"
+        })
+    book = Book.query.filter(Book.name == name).first()
+    if bool(book):
+        return jsonify({
+            "error": "Book is exists"
+        })
+    author = data.get("author","").strip()
+    if not author:
+        return jsonify({
+            "error":"Author is valid"
+        })
+    if author.isdigit():
+        auth = Author.query.get(int(author))
+        if not auth:
+            return jsonify({
+                "error":"Author is valid"
+            })
+    else:
+        auth = Author.query.filter(Author.name == author).first()
+        if auth:
+            author = str(auth.id)
+        else:
+            auth = Author()
+            auth.name = author
+            db.session.add(auth)
+            try:
+                db.session.commit()
+                author = str(auth.id)
+            except:
+                db.session.rollback()
+                return jsonify({
+                    "error":"Could not add book author"
+                })
+
+    book = Book()
+    book.name = name
+    book.source = data['source'].strip() if 'source' in data else ''
+    book.isbn = data['isbn'].strip() if 'isbn' in data else ''
+    book.cover = data['cover'].strip() if 'cover' in data else ''
+    book.author_id = int(author)
+    book.created = datetime.now()
+    
+    db.session.add(book)
+    try:
+        db.session.commit()
+        return jsonify({
+            "name":book.name,
+            "source":book.source,
+            "isbn":book.isbn,
+            "author":book.author.name,
+            "created":str(book.created)
+        })
+    except:
+        db.session.rollback()
+        return jsonify({
+            "error":"Could not create book"
+        })
